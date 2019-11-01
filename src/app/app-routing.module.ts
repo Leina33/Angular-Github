74183e52c7968362dc7b3f7fcf6ff2fe618d5811
Leina33 <dominickrutto@gmail.com>
2019-11-01 12:33:36 +0300

    This is the parent component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import {ProfileComponent} from './profile/profile.component'
 
 
 
 const routes: Routes = [
-  {path:'about', component:AboutComponent}
+  {path:'about', component:AboutComponent},
+  {path:'profile',component:ProfileComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GithubComponent } from './github/github.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    ProfileComponent,
+    GithubComponent
   ],
   imports: [
     BrowserModule,
