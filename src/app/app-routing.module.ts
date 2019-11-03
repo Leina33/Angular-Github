822a84797d6702b198e554691abdee8f1cce89a8
Leina33 <dominickrutto@gmail.com>
2019-11-04 02:19:34 +0300

    Changed the routes of not-found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { GithubComponent } from './github/github.component';
+import { GithubComponent } from './github/github.component';
 import { AboutComponent } from './about/about.component';
 import {ProfileComponent} from './profile/profile.component'
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 const routes: Routes = [
   {path:'about', component:AboutComponent},
-  // {path: 'github', component:GithubComponent},
-  {path:'profile',component:ProfileComponent}
+  {path: 'github', component:GithubComponent},
+  {path:'profile',component:ProfileComponent},
+  {"path":"**","component":NotFoundComponent}
 ];
 
 @NgModule({
