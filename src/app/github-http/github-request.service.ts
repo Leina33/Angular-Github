c514b89551219b85c65f862a6ccb096ff9850d69
Leina33 <dominickrutto@gmail.com>
2019-11-06 09:09:49 +0300

    edited the README.md fie

--- src/app/github-http/github-request.service.ts
+++ src/app/github-http/github-request.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment } from '../../environments/environment';
+import {Github } from '../github';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubRequestService {
+  github:Github[]=[];
+  constructor(private http:HttpClient)
 
   constructor() { }
 }
