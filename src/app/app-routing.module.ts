d34719b718e4a506e09ddfcd5cc3d1ed697c3009
Leina33 <dominickrutto@gmail.com>
2019-11-07 14:54:32 +0300

    created for profile component in app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path:'about', component:AboutComponent},
   {path: 'github', component:GithubComponent},
   {path:'profile',component:ProfileComponent},
-  {"path":"**","component":NotFoundComponent}
+  {"path":"  ","component":NotFoundComponent}
 ];
 
 @NgModule({
